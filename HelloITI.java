47ea70e8f7e254e713c8bde19ea5c83b1268198d
Mariem <mariammuhammad911@gmail.com>
2025-01-12 07:19:44 +0200

    add project

--- HelloITI.java
+++ HelloITI.java
@@ -3,6 +3,7 @@ public class HelloWorld
 	public static void main(string[] args)
 	{
 		System.out.println("Hello ITI");
+		System.out.println("Hello Git");
 	}
 
 }
--- HelloJava.java
+++ HelloJava.java
@@ -1,8 +1,12 @@
 public class HelloWorld
 {
 	public static void main(string[] args)
+
 	{
-		System.out.println("Hello World");
+
+				System.out.println("Hello World");
+				print("Hello Java");
+
 	}
 
 }
